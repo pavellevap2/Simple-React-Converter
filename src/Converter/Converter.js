4240dd5ffc07780acb8848b73b1ac6bd682bf6a1
Your Name <youremail@domain.com>
2018-02-07 16:20:26 +0300

    problemes

--- src/Converter/Converter.js
+++ src/Converter/Converter.js
@@ -1,89 +1,111 @@
 import React from "react";
 import "./Converter.css"
 import arrows from "../assets/images/arrow_two_head.png";
-var debounce = require("lodash.debounce");
+// const debounce = require("lodash.debounce");
 
-let toUSD = (rub) => {
-    return (rub * 0.01752).toFixed(4)
+const currencies = ["USD", "EURO", "RUB", "CNY"];
 
-}
-let toRUB = (usd) =>{
-    return (usd * 57.077).toFixed(4)
-}
+const rate = {
+    "USD": {"RUB" : 57.2196, "EURO" : 0.810431879 ,"CNY" : 6.26919942 },
+    "EURO": {"USD" : 1.23391, "RUB" : 70.6302232 ,"CNY" : 7.73562786 },
+    "RUB": {"USD" : 0.01747, "EURO" : 0.0141582449 ,"CNY" : 0.109522914 },
+    "CNY": {"USD" : 0.15951, "EURO" : 0.129271989  ,"RUB" : 9.13050944 },
+
+};
+
+let convertToCurr = (amount, curr) => {
+    return (amount * curr).toFixed(4)
+} ;
 
 class Converter extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            USDtoRUB :   {id :1, currencies:"USD to RUB"},
-            RUBtoUSD :   { id :2, currencies:"RUB to USD"},
-            currency : {money : "" , costs : ""},
-            fromTo :"",
-            value: "",
-
-        };
-
-       this.makeRubToUSd = this.makeRubToUSd.bind(this);
-        this.makeUSdToRub = this.makeUSdToRub.bind(this);
-        this.makeConversion = this.makeConversion.bind(this);
-    }
-
-    makeUSdToRub(){
-            this.setState({
-                fromTo : this.state.USDtoRUB.id,
-                currency : {money:"RUB" , costs : "0.01759"}
-            })
-    }
-    makeRubToUSd(){
-        this.setState({
-                fromTo : this.state.RUBtoUSD.id,
-                currency : {money:"USD" , costs : "57.077"}
-            })
-    }
-    makeConversion(event){
-        if (this.state.fromTo === this.state.USDtoRUB.id){
-            this.setState({
-                value : toRUB(event.target.value)
-            })
-        } else if (this.state.fromTo === this.state.RUBtoUSD.id){
-            this.setState({
-                value : toUSD(event.target.value)
-            })
+            inputValue : "",
+            outputValue : "",
+            fromCurr : "",
+            toCurr : "",
         }
     }
+    // enterData(e){
+    //     this.setState({
+    //         inputValue : e.target.value
+    //     })
+    // }
+
+    setFromCurr(c){
+        this.setState({
+            fromCurr : c ,
+        })
+    }
+
+    setToCurr(c) {
+       this.setState({
+           toCurr : c
+       })
+    }
+    makeConversion(e){
+        if(this.state.fromCurr === "USD" && this.state.toCurr === "RUB"){
+            this.setState({
+                inputValue : e.target.value,
+                outputValue : convertToCurr(this.state.inputValue, rate.USD.RUB)
+            })
+        }
+
+    }
+
 
     render() {
+
         return(
             <div className="Converter">
-                <h1 className="Converter_title">Simple Converter</h1>
+                <h1 className="Converter_title">Simple Converter </h1>
 
                 <div className="Converter_main">
                     <h3 className="Converter_instruction">Enter a number to convert</h3>
                     <div className="Converter_main_input">
-                            <input  onChange={debounce(this.makeConversion, 250,{"maxWait" : 2000})}  type="text"/>
+                            <input onChange ={ (e) => this.makeConversion(e)}   type="text"/>
                     </div>
                     <div className="Сonverter_main_selection">
                         <form action="">
-                            <p className="Сonverter_main_selection_select">
-                                <p className='Сonverter_main_selection_from-to'>From...TO</p>
+                            <div className="Сonverter_main_selection_select">
+                                <p className='Сonverter_main_selection_from-to'>From</p>
                                 <select name="Choose type">
-                                    <option value="">Choose type : </option>
-                                    <option
-                                        value={this.state.USDtoRUB.currencies} onClick={this.makeUSdToRub}>{this.state.USDtoRUB.currencies}
-                                    </option>
-                                    <option
-                                        value={this.state.RUBtoUSD.currencies} onClick={this.makeRubToUSd}>{this.state.RUBtoUSD.currencies}
-                                    </option>
+                                    <option value="Choose currency">currencies:</option>
+                                    {currencies.map(c =>
+                                        <option onClick={() => this.setFromCurr(c)}>
+                                            {c}
+                                        </option>
+                                    )}
+
                                 </select>
-                            </p>
+                            </div>
+                        </form>
+
+                        <button>
+                            <img src={arrows} alt="arrows"/>
+                        </button>
+
+                        <form action="">
+                            <div className="Сonverter_main_selection_select">
+                                <p className='Сonverter_main_selection_from-to'>TO</p>
+                                <select name="Choose type">
+                                    <option value="Choose currency">currencies:</option>
+                                    {currencies.map(c =>
+                                        <option onClick={() => this.setToCurr(c)}>
+                                            {c}
+                                        </option>
+                                    )}
+                                </select>
+                            </div>
                         </form>
                     </div>
                 </div>
                 <div className="Converter_output">
-                    <p className="Converter_output_value">Conversion:{this.state.value}</p>
-                    <p className="Converter_output_rate">1 {this.state.currency.money} equals {this.state.currency.costs}</p>
-                </div>
 
+                    <p className="Converter_output_value" >Conversion: {this.state.outputValue}</p>
+                    <p className="Converter_output_rate">1 equals </p>
+                </div>
             </div>
         )
     }
